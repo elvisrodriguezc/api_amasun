1fbca8545b35b5942aac7dff83acd25bce016293
elvisrodriguezc <elvisrodriguezc@gmail.com>
2022-11-26 23:39:08 -0500

    actulize the migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        // \App\Models\Location::factory(10)->create();
-        // \App\Models\Boat::factory(10)->create();
-        // \App\Models\Service::factory(10)->create();
-        // \App\Models\Departure::factory(10)->create();
-        // \App\Models\Document::factory(10)->create();
-        // \App\Models\Departamento::factory(24)->create();
-        // \App\Models\Provincia::factory(100)->create();
-        // \App\Models\Distrito::factory(150)->create();
-        // \App\Models\Customer::factory(10)->create();
-        // \App\Models\Booking::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Location::factory(10)->create();
+        \App\Models\Boat::factory(10)->create();
+        \App\Models\Service::factory(10)->create();
+        \App\Models\Departure::factory(10)->create();
+        \App\Models\Document::factory(10)->create();
+        \App\Models\Departamento::factory(24)->create();
+        \App\Models\Provincia::factory(100)->create();
+        \App\Models\Distrito::factory(150)->create();
+        \App\Models\Customer::factory(10)->create();
+        \App\Models\Booking::factory(10)->create();
     }
 }
